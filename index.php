e15b7f51cf9a8ff24eefd9a29538a96e201786f5
Archant <88141@glr.nl>
2023-08-28 09:14:53 +0200

    First commit
    Just making sure

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Responsive Navbar</title>
+</head>
+<body>
+    <nav class="navbar">
+        <ul class="nav-list">
+            <li class="nav-item active"><a href="#">Home</a></li>
+            <li class="nav-item"><a href="#">About</a></li>
+            <li class="nav-item"><a href="#">Services</a></li>
+            <li class="nav-item"><a href="#">Contact</a></li>
+            <!-- Add more <li> elements for additional tabs -->
+        </ul>
+    </nav>
+    <script src="script.js"></script>
+</body>
+</html>
