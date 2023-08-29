c892b3c4d7188357f1ad17b7e0fddf4687356ba0
Archant <88141@glr.nl>
2023-08-29 16:48:17 +0200

    quickfix

--- Projects/projects.php
+++ Projects/projects.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "your_server_name";
-$username = "your_username";
-$password = "your_password";
-$dbname = "your_database_name";
+$servername = "localhost";
+$username = "db88141";
+$password = "Kaas1001!";
+$dbname = "88141_database";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -10,19 +10,33 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM projects";
+$sql = "SELECT * FROM Projects";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
+        echo "<div class='project-card'>";
         echo "<h2>" . $row['name'] . "</h2>";
         echo "<p>" . $row['description'] . "</p>";
-        echo "<img src='" . $row['image_path'] . "' alt='Project Image'>";
-        echo "<a href='" . $row['file_path'] . "' download>Download Project File</a>";
+        echo "<img class='project-image' src='" . $row['image_path'] . "' alt='Project Image'>";
+        echo "<a class='project-download' href='" . $row['file_path'] . "' download>Download Project File</a>";
+        echo "</div>";
     }
 } else {
     echo "No projects found.";
 }
-
 $conn->close();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel='stylesheet' href='Style/project.css'>
+</head>
+<body>
+    
+</body>
+</html>
